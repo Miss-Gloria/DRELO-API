2b47ba944eae69dfd0982a83ddab37da5dec15e0
gloria larbi <Miss-Gloria>
2025-04-10 09:08:55 +0000

    updated the index.js

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import aiRouter from "./routes/openai_routes.js";
 import userRouter from "./routes/user_route.js";
+import { connect } from "mongoose";
 
 
-
-//make databse connection 
-await mongoose.connect(process.env.MONGO_URI);
-console.log('Database connected successfully');
-
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Database connected successfully'))
+  .catch((err) => {
+    console.error('MongoDB connection failed:', err);
+    process.exit(1);
+  });
 
 // Create an express app
 const app = express();
@@ -16,9 +18,14 @@ const app = express();
 //Use global middleware
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Drelo API is running");
+});
 //use routes
 app.use(aiRouter);
 app.use(userRouter);
+
+
 
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
