d6194d5075d31053b1d9d0c44c477d787e7acb46
gloria larbi <Miss-Gloria>
2025-04-26 18:38:48 +0000

    new

--- validators/user_validator.js
+++ validators/user_validator.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 export const registerUserValidator = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
-  password: Joi.string().min(6).pattern(new RegExp("^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,}$")).message("Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.").required(),
+  password: Joi.string().min(5).required(),
   confirmPassword: Joi.ref("password"),
 }).with("password", "confirmPassword");
 
@@ -42,16 +42,7 @@ export const forgotPasswordValidator = Joi.object({
 export const resetPasswordValidator = Joi.object({
   code: Joi.number().required(),
   newPassword: Joi.string()
-    .min(6)
-    .pattern(
-      new RegExp(
-        "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,}$"
-      )
-    )
-    .message(
-      "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character."
-    )
-    .required(),
+    .min(5).required(),
   confirmNewPassword: Joi.ref("newPassword"),
 }).with("newPassword", "confirmNewPassword");
 
